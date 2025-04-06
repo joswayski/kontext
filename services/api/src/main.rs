c58a88e3816cfda5830aac674c87fbfb0552fdd9
Jose Valerio <contact@josevalerio.com>
2025-04-06 18:13:42 +0300

    saving

--- services/api/src/main.rs
+++ services/api/src/main.rs
@@ -1,11 +1,28 @@
 use axum::{routing::get, Router};
+use std::time::Duration;
+use tower::ServiceBuilder;
+use tower_http::compression::CompressionLayer;
+use tower_http::timeout::TimeoutLayer;
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
 async fn main() {
+    // Initialize tracing
+    tracing_subscriber::fmt::init();
+
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .layer(
+            ServiceBuilder::new()
+                // When using a ServiceBuilder, middleware is applied top down
+                .layer(TraceLayer::new_for_http())
+                .layer(CompressionLayer::new())
+                .layer(TimeoutLayer::new(Duration::from_secs(10))),
+        );
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4000").await.unwrap();
+    tracing::info!("Server starting on http://0.0.0.0:4000");
     axum::serve(listener, app).await.unwrap();
 }
