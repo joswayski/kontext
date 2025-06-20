eabcf205f85690a4de63268754fcea15f9ca95e6
Jose Valerio <contact@josevalerio.com>
2025-06-20 18:40:54 -0400

    saving

--- services/web/app/routes/home.tsx
+++ services/web/app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Kontext by Jose Valerio" },
+    { name: "description", content: "Automated Kafka event flow visualization and business logic mapping" },
   ];
 }
 
