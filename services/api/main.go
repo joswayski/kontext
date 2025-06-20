9433659cc02c6db6a7f6775833396dca02cf7d6d
Jose Valerio <contact@josevalerio.com>
2025-06-20 18:39:48 -0400

    saving

--- services/api/main.go
+++ services/api/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello")
+
+	})
+	r.Run("0.0.0.0:4000")
 }
