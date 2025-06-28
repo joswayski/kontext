ed0f4e6d778a087a09e218cfe46caa3f069499d6
Jose Valerio <contact@josevalerio.com>
2025-06-28 17:52:17 -0400

    saving

--- apps/api/main.go
+++ apps/api/main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/joswayski/kontext/apps/api/config"
 	"github.com/joswayski/kontext/apps/api/router"
 )
@@ -9,5 +17,33 @@ func main() {
 	cfg := config.GetConfig()
 	r := router.GetRouter()
 
-	r.Run(":" + cfg.Port)
+	srv := &http.Server{
+		Addr:    ":" + cfg.Port,
+		Handler: r,
+	}
+
+	go func() {
+		slog.Info("Starting API server on port " + cfg.Port)
+		err := srv.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			slog.Error("Error running API server", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	// Wait for shutdown signal
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	slog.Warn("Shutting down API server")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	err := srv.Shutdown(ctx)
+	if err != nil {
+		slog.Error("Server forced to shutdown:", "error", err)
+		os.Exit(1)
+	}
+
+	slog.Info("API server shutdown complete")
 }
