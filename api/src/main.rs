b209773a72b4ae72fcb63e7f6aa545430bb25d83
Jose Valerio <contact@josevalerio.com>
2025-08-24 14:12:20 -0400

    saving

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,10 @@
+use shared::load_env;
+use std::env;
 fn main() {
+    load_env();
     println!("Hello from API!");
+
+    for (key, value) in env::vars() {
+        println!("Key: {} - Value: {}", key, value)
+    }
 }
